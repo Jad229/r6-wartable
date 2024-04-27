5b06583ac2d52f638ce7b9f6c1db53fdfd72a535
Justin Diaz <justhewebdev@gmail.com>
2024-04-27 17:44:05 -0400

    separated operators to own module

---
+++ src/js/operators.ts
@@ -0,0 +1,13 @@
+import BuckIcon from "../assets/operators/buck.png";
+import HibanaIcon from "../assets/operators/hibana.png";
+import KapkanIcon from "../assets/operators/kapkan.png";
+import MuteIcon from "../assets/operators/mute.png";
+import ThermiteIcon from "../assets/operators/thermite.png";
+
+export const operators = [
+  { name: "Buck", icon: BuckIcon },
+  { name: "Hibana", icon: HibanaIcon },
+  { name: "Kapkan", icon: KapkanIcon },
+  { name: "Mute", icon: MuteIcon },
+  { name: "Thermite", icon: ThermiteIcon },
+];
