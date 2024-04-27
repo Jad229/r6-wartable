e737055e0a4605fa4121929432881b800ac985b7
Justin Diaz <justhewebdev@gmail.com>
2024-04-27 17:01:32 -0400

    style: fixed operator images to below map

--- src/js/index.ts
+++ src/js/index.ts
@@ -13,5 +13,26 @@ const operatorList = document.querySelector(".operator-list");
 
 const BuckImage: HTMLImageElement = document.createElement("img");
 BuckImage.setAttribute("src", BuckIcon);
+BuckImage.classList.add("operator");
+
+const HibanaImage: HTMLImageElement = document.createElement("img");
+HibanaImage.setAttribute("src", HibanaIcon);
+HibanaImage.classList.add("operator");
+
+const KapkanImage: HTMLImageElement = document.createElement("img");
+KapkanImage.setAttribute("src", KapkanIcon);
+KapkanImage.classList.add("operator");
+
+const MuteImage: HTMLImageElement = document.createElement("img");
+MuteImage.setAttribute("src", MuteIcon);
+MuteImage.classList.add("operator");
+
+const ThermiteImage: HTMLImageElement = document.createElement("img");
+ThermiteImage.setAttribute("src", ThermiteIcon);
+ThermiteImage.classList.add("operator");
 
 operatorList?.appendChild(BuckImage);
+operatorList?.appendChild(HibanaImage);
+operatorList?.appendChild(KapkanImage);
+operatorList?.appendChild(MuteImage);
+operatorList?.appendChild(ThermiteImage);
