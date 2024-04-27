fd2ad03e16641fe982d698d6ef403322858d10a5
Justin Diaz <justhewebdev@gmail.com>
2024-04-27 18:49:04 -0400

    Add operator icons and click event listener

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,7 +1,27 @@
 import CoastlineImage from "../assets/maps/CoastlineTopFloorBG.jpg";
 import "../styles.css";
+import { operators } from "./operators";
 
 const map: HTMLElement | null = document.getElementById("map");
 map?.setAttribute("src", CoastlineImage);
 
-const operatorList = document.querySelector(".operator-list");
+const operatorList: HTMLDivElement | null =
+  document.querySelector(".operator-list");
+
+operators.forEach((op) => {
+  const img: HTMLImageElement = document.createElement("img");
+
+  img.src = op.icon;
+  img.alt = op.name;
+  img.classList.add("operator");
+
+  img.addEventListener("click", () => {
+    selectOperator(op.name);
+  });
+
+  operatorList?.appendChild(img);
+});
+
+function selectOperator(name: string) {
+  const operatorImg = document.createElement("img");
+}
