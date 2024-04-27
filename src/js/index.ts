ce2575d3916905b86cdc9de50eacfbf193803154
Justin Diaz <justhewebdev@gmail.com>
2024-04-27 16:37:48 -0400

    added operator images

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,17 @@
 import CoastlineImage from "../assets/maps/CoastlineTopFloorBG.jpg";
+import BuckIcon from "../assets/operators/buck.png";
+import HibanaIcon from "../assets/operators/hibana.png";
+import KapkanIcon from "../assets/operators/kapkan.png";
+import MuteIcon from "../assets/operators/mute.png";
+import ThermiteIcon from "../assets/operators/thermite.png";
 import "../styles.css";
 
 const map: HTMLElement | null = document.getElementById("map");
 map?.setAttribute("src", CoastlineImage);
+
+const operatorList = document.querySelector(".operator-list");
+
+const BuckImage: HTMLImageElement = document.createElement("img");
+BuckImage.setAttribute("src", BuckIcon);
+
+operatorList?.appendChild(BuckImage);
